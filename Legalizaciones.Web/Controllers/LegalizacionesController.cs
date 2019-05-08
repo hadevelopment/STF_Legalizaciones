fdd98d65668908a96ec05facbfe160699103041f
EfrainMejiasC <efrainmejiasc@hotmail.com>
2019-05-08 14:12:36 -0400

    INDEX

---
+++ Legalizaciones.Web/Controllers/LegalizacionesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Legalizaciones.Web.Controllers
+{
+    public class LegalizacionesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
