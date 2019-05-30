c04324dad6a27f1906e4ba15ff37c55ce31accc5
jaxierro <jaxier.rod@outlook.com>
2019-05-30 08:41:24 -0400

    Validaciones

--- Legalizaciones.Web/Models/ViewModel/SolicitudTDCViewModel.cs
+++ Legalizaciones.Web/Models/ViewModel/SolicitudTDCViewModel.cs
@@ -11,12 +11,14 @@ namespace Legalizaciones.Web.Models.ViewModel
     public class SolicitudTDCViewModel
     {
         [Required(ErrorMessage = "Debe indicar una Cedula.")]
+        [RegularExpression(@"^[a-zA-Z0-9]{4,10}$")]
         public string Cedula {get;set;}
 
         [Required(ErrorMessage = "Debe indicar una Tarjeta de Credito.")]
         public string Extracto { get; set; }
 
         [Required(ErrorMessage = "Debe indicar un nombre de Banco.")]
+        [RegularExpression(@"^[a-zA-Z0-9]{4,10}$", ErrorMessage = "Debe indicar un nombre del Banco valido.")]
         public string Banco { get; set; }
 
         [Range(1, (double)decimal.MaxValue, ErrorMessage = "Debe indicar un Monto valido.")]
