316a2c7867cabd0ac15721fb025bf79af3089ea7
efrainmejiasc <efrainmejiasc@hotmail.com>
2019-06-12 12:30:12 -0400

    ERPObjects

---
+++ Legalizaciones.Core/Model/ERP/ServiceType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Legalizaciones.Model.ERP
+{
+    //WS_TIPOS_SERVICIOS
+    [XmlRoot(ElementName = "Resultado")]
+    public class ServiceType
+    {
+        [XmlElement(ElementName = "idTipoServicio")]
+        public string IdTipoServicio { get; set; }
+
+        [XmlElement(ElementName = "Nombre")]
+        public string Nombre { get; set; }
+
+        [XmlElement(ElementName = "Contabilizacion_x0020_costo")]
+        public string Contabilizacion_X0020_Costo { get; set; }
+
+        [XmlElement(ElementName = "Clase")]
+        public string Clase { get; set; }
+
+        [XmlAttribute(AttributeName = "id", Namespace = "urn:schemas-microsoft-com:xml-diffgram-v1")]
+        public string Id { get; set; }
+    }
+
+    [XmlRoot(ElementName = "NewDataSet")]
+    public class ListServiceType
+    {
+        [XmlElement(ElementName = "Resultado")]
+        public List<ListServiceType> Resultado { get; set; }
+    }
+}
---
+++ Legalizaciones.Core/Model/ERP/Suppliers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Legalizaciones.Model.ERP
+{
+    //WS_PROVEEDORES
+    [XmlRoot(ElementName = "Resultado")]
+    public class Suppliers
+    {
+        [XmlElement(ElementName = "idProveedor")]
+        public string IdProveedor{ get; set; }
+
+        [XmlElement(ElementName = "Nombre")]
+        public string Nombre { get; set; }
+
+        [XmlElement(ElementName = "telefono")]
+        public string Telefono { get; set; }
+
+        [XmlElement(ElementName = "celular")]
+        public string Celular { get; set; }
+
+        [XmlElement(ElementName = "FechaReg")]
+        public string FechaReg { get; set; }
+
+        [XmlElement(ElementName = "sucursal")]
+        public string Sucursal { get; set; }
+
+        [XmlElement(ElementName = "des_sucursal")]
+        public string Des_Sucuursal { get; set; }
+
+        [XmlElement(ElementName = "pais")]
+        public string Pais { get; set; }
+
+        [XmlElement(ElementName = "depto")]
+        public string Depto { get; set; }
+
+        [XmlElement(ElementName = "ciudad")]
+        public string Ciudad { get; set; }
+
+        [XmlElement(ElementName = "direccion")]
+        public string Direccion { get; set; }
+
+        [XmlElement(ElementName = "email")]
+        public string Email { get; set; }
+
+        [XmlAttribute(AttributeName = "id", Namespace = "urn:schemas-microsoft-com:xml-diffgram-v1")]
+        public string Id { get; set; }
+    }
+
+
+    [XmlRoot(ElementName = "NewDataSet")]
+    public class ListUnidadNegocio
+    {
+        [XmlElement(ElementName = "Resultado")]
+        public List<Suppliers> Resultado { get; set; }
+    }
+
+}
